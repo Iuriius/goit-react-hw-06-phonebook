870e7283db23881a54bc8885329e38f68afcdf03
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-16 16:12:52 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
+import { Section, Title } from './App.styled';
+import { Toaster } from 'react-hot-toast';
+import { useSelector } from 'react-redux';
+import { Form } from './Form/Form';
+import { Filter } from './Filter/Filter';
+import { ContactsList } from './ContactsList/ContactsList';
+import { contactSelector } from '../redux/selectors';
+
 export const App = () => {
+  const { contacts } = useSelector(contactSelector);
+
+  // const handleChange = data => {
+  //   const newContact = {
+  //     id: nanoid(),
+  //     ...data,
+  //   };
+
+  //   if (takenName(newContact.name)) {
+  //     return toast.error(`${newContact.name} is already in contacts`);
+  //   }
+
+  //   dispatch(addContact(newContact));
+  // };
+
+  // const takenName = contactName =>
+  //   contacts.find(contact => contact.name === contactName);
+
+  // const handleDelete = id => {
+  //   dispatch(deleteContact(id));
+  // };
+
+  // const handleFilter = ({ target: { value } }) => {
+  //   dispatch(filterContacts(value));
+  // };
+
+  // const getContacts = () => {
+  //   const normalizedFilter = filter.toLowerCase();
+  //   return contacts.filter(({ name }) =>
+  //     name.toLowerCase().includes(normalizedFilter)
+  //   );
+  // };
+
+  // const currentContacts = getContacts();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div>
+        <Toaster />
+      </div>
+      <Section>
+        <Title>Phonebook</Title>
+        <Form />
+      </Section>
+      <Section>
+        {!!contacts.length && (
+          <>
+            <Title>Contacts</Title>
+            <Filter />
+            <ContactsList />
+          </>
+        )}
+      </Section>
+    </>
   );
 };
