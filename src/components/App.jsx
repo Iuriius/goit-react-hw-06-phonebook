d13f9da8e4fbd951a9ddb58575fc6cda473eaa30
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-02-17 09:59:27 +0200

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,39 +9,6 @@ import { contactSelector } from '../redux/selectors';
 export const App = () => {
   const { contacts } = useSelector(contactSelector);
 
-  // const handleChange = data => {
-  //   const newContact = {
-  //     id: nanoid(),
-  //     ...data,
-  //   };
-
-  //   if (takenName(newContact.name)) {
-  //     return toast.error(`${newContact.name} is already in contacts`);
-  //   }
-
-  //   dispatch(addContact(newContact));
-  // };
-
-  // const takenName = contactName =>
-  //   contacts.find(contact => contact.name === contactName);
-
-  // const handleDelete = id => {
-  //   dispatch(deleteContact(id));
-  // };
-
-  // const handleFilter = ({ target: { value } }) => {
-  //   dispatch(filterContacts(value));
-  // };
-
-  // const getContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-  //   return contacts.filter(({ name }) =>
-  //     name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-
-  // const currentContacts = getContacts();
-
   return (
     <>
       <div>
